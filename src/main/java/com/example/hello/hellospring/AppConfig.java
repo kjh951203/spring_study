ae3e47429f0a4f719d196002a83e230e84600c73
junghwan <kjh951203#gmail.com>
2022-10-13 04:43:26 -0700

    second

--- src/main/java/com/example/hello/hellospring/AppConfig.java
+++ src/main/java/com/example/hello/hellospring/AppConfig.java
@@ -13,7 +13,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
-
     @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(MemberRepository());
